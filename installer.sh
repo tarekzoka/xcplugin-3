eb5b11d8b2b6d2420f279031a6c924c765f261de
emil nabil <elbrinselomda@yahoo.com>
2021-09-11 08:38:30 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,24 +1,63 @@
 #!/bin/sh
 #
-#command wget https://raw.githubusercontent.com/emilnabil/xcplugin/main/installer.sh -O - | /bin/sh 
+#command wget https://raw.githubusercontent.com/emilnabil/xcplugin/main/installer.sh -O - | /bin/sh  
 #
+#
+#
+echo ' download and install plugin xcplugin '
+echo ' ====================================================================='
 wget -O /var/volatile/tmp/xcplugin.tar.gz "https://raw.githubusercontent.com/emilnabil/xcplugin/main/xcplugin.tar.gz"
 wait 
-tar xzvpf /tmp/xcplugin.tar.gz  -C /
+tar xzvpf /tmp/*.tar.gz  -C /
 wait
 rm -r /var/volatile/tmp/xcplugin.tar.gz                         
-                       
+
 echo "========================================================================"
                 
-	echo ".      WELCOME TO PLUGIN xcplugin "
+	echo ".  WELCOME TO PLUGIN XCPLUGIN "
 	
-echo "#    plugin INSTALLED SUCCESSFULLY           #"
+echo "#    PLUGIN INSTALLED SUCCESSFULLY           #"
 echo "        UPLOADED BY EMIL_NABIL  "     
 echo "#########################################################"
 echo "#########################################################"
 wait
 sleep 2
 exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
