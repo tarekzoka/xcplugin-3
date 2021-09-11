2e477dc8223c001331fc0fade9fcfe562b9a3170
emil nabil <elbrinselomda@yahoo.com>
2021-09-11 09:32:47 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -2,15 +2,21 @@
 #
 #command wget https://raw.githubusercontent.com/emilnabil/xcplugin/main/installer.sh -O - | /bin/sh
  #
-wget -O /var/volatile/tmp/xcplugin.tar.gz "https://raw.githubusercontent.com/emilnabil/xcplugin/main/xcplugin.tar.gz"
+wget -O /var/volatile/tmp/xcplugin-1.6.tar.gz "https://raw.githubusercontent.com/emilnabil/xcplugin/main/xcplugin-1.6.tar.gz"
 wait
 tar xzvpf /tmp/*.tar.gz -C /
 wait
-rm -r /var/volatile/tmp/xcplugin.tar.gz 
-echo "========================================================================" 	echo ". WELCOME TO PLUGIN XCPLUGIN "	echo "# PLUGIN INSTALLED SUCCESSFULLY #"echo " UPLOADED BY EMIL_NABIL " echo "#########################################################"echo "#########################################################"
+rm -r /var/volatile/tmp/xcplugin-1.6.tar.gz 
+echo "========================================================================" 	
+echo ". WELCOME TO PLUGIN XCPLUGIN "
+	echo " PLUGIN INSTALLED SUCCESSFULLY "
+echo " UPLOADED BY EMIL_NABIL " 
+echo "#########################################################"
+echo "#########################################################"
 wait
 sleep 2
 exit 0
+
 
 
 
