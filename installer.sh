fa6b5a50d85954ac7eb0a92af8c26c4148d910af
emil nabil <elbrinselomda@yahoo.com>
2021-09-14 18:57:28 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,35 +1,91 @@
 #!/bin/sh
-# ###########################################
+#
 # SCRIPT : DOWNLOAD AND INSTALL xcplugin
-# ###########################################
 #
-# Command: wget https://raw.githubusercontent.com/emilnabil/xcplugin/main/installer.sh -qO - | /bin/sh 
-#
-# ###########################################
-
-
 ###########################################
-MY_URL="https://raw.githubusercontent.com/emilnabil/xcplugin/main/"  
-
-echo " download and install plugin "
-
+#command: wget https://raw.githubusercontent.com/emilnabil/xcplugin/main/installer.sh -O - | /bin/sh
+ #
+MY_EM="*********************************************************************************"
 wget -O /var/volatile/tmp/xcplugin-1.6.tar.gz "https://raw.githubusercontent.com/emilnabil/xcplugin/main/xcplugin-1.6.tar.gz"
 wait
-tar xzvpf /tmp/xcplugin-1.6.tar.gz  -C /
+echo $MY_EM
+######      INSTALL PLUGIN Xcplugin ###
+tar xzvpf /tmp/*.tar.gz -C /
 wait
-sleep 2;
-echo "" 
-echo "" 
-echo "" "*********************************************************" 
-echo "# DOWNLOAD AND INSTALLED SUCCESSFULLY #"
-echo "          UPLOADED BY EMIL_NABIL "
-echo " "*********************************************************" 
-echo "#.        YOUR DEVICE WELL RESTART Now #" 
+echo $MY_EM
+#######   REMOVE PACKAGE FROM TMP ###
+rm -r /var/volatile/tmp/xcplugin-1.6.tar.gz 
+echo "========================================================================" 	
+echo ". WELCOME TO PLUGIN XCPLUGIN "
+echo $MY_EM
+	echo " PLUGIN INSTALLED SUCCESSFULLY "
+echo " UPLOADED BY EMIL_NABIL " 
+echo "======================================================"
+echo "       YOUR DEVICE WELL RESTART Now " 
 echo "****************************************"
-wait 2
+wait 3;
 init 4
 init 3
 exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
