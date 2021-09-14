5b3b7d98f7c3a5630268b982f99bb36a3c48df65
emil nabil <elbrinselomda@yahoo.com>
2021-09-14 20:57:23 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,52 +1,83 @@
 #!/bin/sh
-
-# 
-# SCRIPT : DOWNLOAD AND INSTALL xcplugin #
-# 
-# Command: wget https://raw.githubusercontent.com/emilnabil/xcplugin/main/installer.sh -O - | /bin/sh #
-######## 
-# my_config #
-MY_TMP="/tmp"
-VERSION="6.7"
-PACKAGE_DIR="enigma2-plugin-extensions-xcplugin-iptv-mod-lululla"
-MY_URL="https://raw.githubusercontent.com/emilnabil/xcplugin/main/$PACKAGE_DIR_${VERSION}all.ipk"
-######################
-MY_URL="https://raw.githubusercontent.com/emilnabil/xcplugin/main/$PACKAGE_DIR_${VERSION}all.deb"
-
-MY_EM="***************************************************************************"
-################################################################
-	echo $MY_EM
-echo ''
-echo '**  STARTED  **'                                      
-echo ''
-
-# Remove previous  any file #
-rm -f $MY_TMP > /dev/null 2>&1
+#
+# SCRIPT : DOWNLOAD AND INSTALL xcplugin
+#
+###########################################
+#command: wget https://raw.githubusercontent.com/emilnabil/xcplugin/main/installer.sh -O - | /bin/sh
+ #
+MY_EM="*********************************************************************************"
+wget -O /var/volatile/tmp/xcplugin-1.6.tar.gz "https://raw.githubusercontent.com/emilnabil/xcplugin/main/xcplugin-1.6.tar.gz"
+wait
 echo $MY_EM
+######      INSTALL PLUGIN Xcplugin ###
+tar xzvpf /tmp/*.tar.gz -C /
+wait
+echo $MY_EM
+#######   REMOVE PACKAGE FROM TMP ###
+rm -r /var/volatile/tmp/xcplugin-1.6.tar.gz 
+echo "========================================================================" 	
+echo ". WELCOME TO PLUGIN XCPLUGIN "
+echo $MY_EM
+	echo " PLUGIN INSTALLED SUCCESSFULLY "
+echo " UPLOADED BY EMIL_NABIL " 
+echo "======================================================"
+echo "       YOUR DEVICE WELL RESTART Now " 
+echo "****************************************"
+wait 3;
+init 4
+init 3
+exit 0
 
-echo 'Downloading '$MY_FILE' ...'
 
-    echo ".       Please Wait ......"
-    wget $MY_URL${PACKAGE}_${VERSION}all.deb -qP $MY_TMP
-    dpkg install $MY_TMP/${PACKAGE}_${VERSION}all.deb
-echo ""
-wget $MY_URL${PACKAGE}_${VERSION}_all.ipk -qP $MY_TMP
-    opkg install $MY_TMP/${PACKAGE}_${VERSION}_all.ipk
 
-############################
-echo " ============================================================================"
-	echo $MY_EM
-echo ""
-		echo "     SUCCESSFULLY INSTALLED   "
-  echo $MY_EM
-		echo ''
-		echo "         RESTARING  ENIGMA     "
-			init 4; 
-sleep 4; 
-init 3;
-	exit 0
-############              
-	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
